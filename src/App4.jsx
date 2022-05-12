9370beb173065339392a1b66dc043a1baebd68f7
Zed0x01 <sherifashrafmedia@gmail.com>
2022-05-12 09:53:16 +0200

    Second

---
+++ src/App4.jsx
@@ -0,0 +1,36 @@
+import React  from 'react'
+import Header from './App4/Header'
+import Nav from './App4/Nav'
+import Footer from './App4/Footer'
+import Home from './App4/Home'
+import PostPage from './App4/PostPage'
+import EditPost from './App4/EditPost'
+import NewPost from './App4/NewPost'
+import About from './App4/About'
+import Missing from './App4/Missing'
+import { Route , Routes } from 'react-router-dom'
+import { DataProvider } from './context/dataContext'
+
+const App4 = () => {
+  return (
+      <div className="App">
+        
+            <Header title="React JS Blog"/>
+            <DataProvider>
+            <Nav />
+            <Routes >
+              <Route path='/' element={<Home />} />
+              <Route path='/post' element={<NewPost  />}/>
+              <Route path='/post/:id' element={<PostPage />} />
+              <Route path='/editpost/:id' element={<EditPost />} />
+              <Route path='/about' element={<About />} />
+              <Route path='*' element={<Missing />} />
+            </Routes>
+            </DataProvider>
+            <Footer />
+        
+      </div>
+  )
+}
+
+export default App4
---
+++ src/App4/PostPage.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useParams,Link } from 'react-router-dom'
+import { useContext } from 'react';
+import DataContext from '../context/dataContext';
+import api from '../App4/api/posts'
+const PostPage = () => {
+  const {posts , setPosts} = useContext(DataContext);
+  const {id} =useParams();
+  const post = posts.find(post => (post.id).toString() == id);
+
+  const handleDelete= async (id)=>{
+    try{
+    await api.delete(`/posts/${id}`);
+    const postLists = posts.filter( ele => ele.id != id);
+    setPosts(postLists);
+    }catch(err){
+      console.log(`Error: ${err.message}`); 
+    }
+  }
+  return (
+    <main className='PostPage'>
+      <article className='post'>
+        {post && 
+          <>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.datetime}</p>
+            <p className='postBody'>{post.body}</p>
+            <Link to={`/editpost/${post.id}`}><button className='editButton'>Edit Post</button></Link>
+            <button className='deleteButton' onClick={()=>handleDelete(post.id)}>
+              Delete Post
+            </button>
+          </>
+        }
+        {!post &&
+          <>
+            <h2>Post Not Found</h2>
+            <p className='postDate'>Well Done !</p>
+            <Link to='/'>Visit Our Home Page</Link>
+          </>
+        }
+      </article>
+    </main>
+  )
+}
+
+export default PostPage
